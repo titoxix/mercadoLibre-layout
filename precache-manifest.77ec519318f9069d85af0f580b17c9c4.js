cae93814dde15d96c787f52fe5d6b23b107d0d3d
Matias Martinez <mat.360z@gmail.com>
2019-03-19 20:29:47 -0300

    Updates

---
+++ precache-manifest.77ec519318f9069d85af0f580b17c9c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mercadoLibre-layout/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c89b38b211abae1e9489",
+    "url": "/mercadoLibre-layout/static/js/runtime~main.8bb9a401.js"
+  },
+  {
+    "revision": "de98d6d1390b65e694a4",
+    "url": "/mercadoLibre-layout/static/js/main.26cb035f.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/mercadoLibre-layout/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "de98d6d1390b65e694a4",
+    "url": "/mercadoLibre-layout/static/css/main.0bfa54a3.chunk.css"
+  },
+  {
+    "revision": "012eae3d0c15723ec2e0f8018636e31a",
+    "url": "/mercadoLibre-layout/index.html"
+  }
+];
